f5c0d312ddbfc9a305dd524b634d42834eb31384
Oscarintween <oscarintween@gmail.com>
2022-09-13 18:49:25 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,75 @@
+import {useState, useEffect} from 'react'
 import './App.css';
+import fields from './data';
 
 function App() {
+  const [originalOptions, setOriginalOptions] = useState([])
+  const [inputFields, setInputFields] = useState([])
+
+  useEffect(()=>{
+    setInputFields(fields)
+    setOriginalOptions(fields)
+  },[])
+
+  const addField = (e)=>{
+    const id = e.target.value
+    const newInput = originalOptions.filter(input=>input._uid === id)
+    console.log(newInput)
+    setInputFields([...inputFields,newInput[0]])
+  }
+  const removeField = (index)=>{
+    let data = [...inputFields];
+    data.splice(index, 1)
+    console.log(data)
+    setInputFields(data)
+    console.log(inputFields)
+  }
+ 
   return (
+    
     <div className="App">
-     <h1>Dynamic Form</h1>
+      <h1>Dynamic Form</h1>
+      <div className='form'>
+        <div className='form__forms-rendered'>
+        {inputFields && inputFields.map((input,index) =>{
+          return(
+            <div className='rendered-form-container' key={index}>
+              <div>
+                <form> 
+                  <label htmlFor={input.name}>
+                    {input.label}
+                  </label> 
+                  <input
+                    type={input.type}
+                    name={input.name}
+                    placeholder={input.placeholder}
+                    value={input.name}
+                  />
+                </form>
+              </div>
+              <div>
+                <button className='remove-button' onClick={()=>removeField(index)}>X</button>
+              </div>
+            </div>
+            
+          )
+        }
+      )}
+        </div>
+        <div className='form-options'>
+        <label id='choose-option' htmlFor="input-select">Choose an input field to add</label>
+          <select onChange={(e)=>addField(e)} name="inputs" id="input-select">
+              <option value=""> --Please choose an option--</option>
+              <option value="1"> First Name </option>
+              <option value="2"> Last Name</option>
+              <option value="3"> Email </option>
+              <option value="4">Password</option>
+              <option value="5">Gender</option>
+              <option value="6">Goldfish</option>
+          </select>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.jsx
@@ -0,0 +1,43 @@
+const fields = [
+    {
+        component: "text",
+        label:"First Name",
+        placeholder:"John",
+        type: "text",
+        _uid:'1'    
+    },
+    {
+        component: "text",
+        label:"Last Name",
+        placeholder:"Doe",
+        type: "text",
+        _uid:'2'    
+    },
+    {
+        component: "text",
+        label:"Email",
+        placeholder:"JohnDoe@mail.com",
+        type: "email",
+        _uid:'3'    
+    },
+    {
+        component: "text",
+        label:"Password",
+        placeholder:"Password",
+        type: "password",
+        _uid:'4'    
+    },
+    {
+        component: "text",
+        label:"Gender",
+        type: "select",
+        _uid:'5'    
+    },
+    {
+        component: "text",
+        label:"First Name",
+        type: "text",
+        _uid:'6'    
+    },
+]
+export default fields
